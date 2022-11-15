71638767716ed4b3d64aa8a20aae31c3b539e1a4
Celina Ye <celinaye@celinas-mbp.mynetworksettings.com>
2022-11-15 18:05:14 -0500

    fixed radio buttons

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Development
 
 ### Link to Deployed Website
-If you used the stencil code, this is `https://<your GitHub username>.github.io/<name of your repository>`
+
+If you used the stencil code, this is `https://hungrysloth344.github.io/Development`
 
 ### Goal and Value of the Application
+
+The goal of the application is for users to view different animals and add or remove them from their favorites. The user can also sort by the name of the animal from A-Z, Z-A, or none at all. There is also functionality to filter by the animal's active time (Diurnal or Nocturnal) and animal_type (ambhibian, bird, mammal, or reptile). Lastly, it also aggregates the total maximum length of the animal. The value to the user is the ability to explore and learn about cool animals and favorite them.
 
 ### Usability Principles Considered
 
@@ -12,4 +15,3 @@ If you used the stencil code, this is `https://<your GitHub username>.github.io/
 ### How Data is Passed Down Through Components
 
 ### How the User Triggers State Changes
-
--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./assets/animals-data.json";
+import DisplayList from "./components/DisplayList";
+import { useState } from "react";
+import FilteredList from "./FilteredList";
+import FavoritesList from "./FavoritesList";
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [myData, setMyData] = useState(data);
+  const [myFavorites, setMyFavorites] = useState([]);
+  const [cart, setCart] = useState(new Array(data.length).fill(0));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <strong className="main">Cool Animals</strong>
+      <div className="App-Container">
+        <div className="BakeryItem-Container">
+          {myData.map((item, index) => (
+            <DisplayList
+              item={item}
+              index={index}
+              total={total}
+              setTotal={setTotal}
+              key={index}
+              myFavorites={myFavorites}
+              setMyFavorites={setMyFavorites}
+              setCart={setCart}
+            />
+          ))}
+        </div>
+        <div className="filters-card">
+          <div>
+            <FilteredList
+              data={data}
+              myData={myData}
+              setData={setMyData}
+              myFavorites={myFavorites}
+              setMyFavorites={setMyFavorites}
+            />
+          </div>
+
+          <p>Favorites Max Length: {Math.round(total * 100) / 100}</p>
+
+          {/* <div className="filters-card">
+            <div>My Favorites</div>
+            <div>
+              <FavoritesList
+                data={data}
+                myData={myData}
+                setData={setMyData}
+                myFavorites={myFavorites}
+                setMyFavorites={setMyFavorites}
+                total={total}
+                setTotal={setTotal}
+              />
+            </div>
+          </div> */}
+
+          {cart.map((key, idx) => {
+            if (key > 0) {
+              return <div key={idx}>{data[idx].name}</div>;
+            }
+          })}
+        </div>
+      </div>
     </div>
   );
 }
